3629d11e60bac3e514da70cd9ddb08490abc209f
qizles <bPW++123>
2018-01-07 16:25:40 +0100

    genereic views

--- testproject/polls/models.py
+++ testproject/polls/models.py
@@ -23,4 +23,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
--- testproject/polls/views.py
+++ testproject/polls/views.py
@@ -1,31 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from .models import Question, Choice
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.http import Http404
-from django.template import loader
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+from django.views import generic
+from .models import Choice, Question
 
 
 def vote(request, question_id):
@@ -45,3 +22,22 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
