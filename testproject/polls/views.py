83bfd6c38602fcd26ed861746ef60c96d9b96487
qizles <bPW++123>
2018-01-07 01:35:09 +0100

    index template

--- testproject/polls/views.py
+++ testproject/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
@@ -17,3 +18,14 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+from django.template import loader
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
